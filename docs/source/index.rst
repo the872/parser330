ece24388092459e0eb0734d588a0b8f6a0ddda4c
Aid Idrizovic <idrizovicaid@gmail.com>
2016-02-03 22:49:17 -0600

    initial commit

---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+parser330
+=========
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+.. only:: html
+
+   Indices and tables
+   ==================
+
+   * :ref:`genindex`
+   * :ref:`modindex`
+   * :ref:`search`
---
+++ parser330/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""A simple Twitter Tweet parser using regular expressions to retrieve data."""
+
+from parser330 import metadata
+
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
