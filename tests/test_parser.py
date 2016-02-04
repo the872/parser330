f73fc08a7b92d2a23ace083afc61fff5e6ebb2ed
Aid Idrizovic <idrizovicaid@gmail.com>
2016-02-04 17:51:33 -0600

    added more tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -10,21 +10,41 @@ class TestParser(unittest.TestCase):
         self.parser = parser330.Parser()
 
     def test_url(self):
-        test = self.parser.parse('so here is this cool url for a cool site called google. http://google.com')
-        self.assertEqual(test.url, 'http://google.com/')
+        test0 = self.parser.parse('so here is this cool url for a cool site called google. http://google.com')
+        self.assertEqual(test0.url, 'http://google.com')
+        test1 = self.parser.parse('here is as newhttp:jknoot .com #@ http://google.com for this')
+        self.assertEqual(test1.url, 'http://google.com')
+        test2 = self.parser.parse('lets do one with no link to see if its going well')
+        self.assertEqual(test2.url, '')
 
     def test_hastag(self):
-        test = self.parser.parse('so now we have all of these cool hashtags #hashtags')
-        self.assertEqual(test.hashtag, '#hastags')
+        test0 = self.parser.parse('so now we have all of these cool hashtags #hashtags')
+        self.assertEqual(test0.hashtag, '#hastags')
+        test1 = self.parser.parse('google.#hastag son')
+        self.assertEqual(test1.hashtag, 'hashtag')
+        test2 = self.parser.parse('lets see what it does when there are no hashtags')
+        self.assrtEqual(test2.hashtag, '')
 
     def test_users(self):
-        test = self.parser.parse('great, no we only have to test a couple other things @the872')
-        self.assertEqual(test.user, '@the872')
+        test0 = self.parser.parse('great, no we only have to test a couple other things @the872')
+        self.assertEqual(test0.user, '@the872')
+        test1 = self.parser.parse('http:notaurl@the872')
+        self.assertEqual(test1.user, '@the872')
+        test2 = self.parser.parse('no users to find here')
+        self.assertEqual(test2.user, '')
 
     def test_parser(self):
-        test = self.parser.parse('lets #make @sure http://everythingworks.com')
-        self.assertEqual(test.user, '@sure')
-        self.assertEqual(test.hashtag, '#make')
-        self.assertEqual(test.url, 'http://everythingworks.com')
+        test0 = self.parser.parse('lets #make @sure http://everythingworks.com')
+        self.assertEqual(test0.user, '@sure')
+        self.assertEqual(test0.hashtag, '#make')
+        self.assertEqual(test0.url, 'http://everythingworks.com')
+        test1 = self.parser.parse('what #if @things are missing')
+        self.assertEqual(test1.user, '@things')
+        self.assertEqual(test1.hashtag, '#if')
+        self.assertEqual(test1.url, '')
+        test2 = self.parser.parse('nothing to parse here')
+        self.assertEqual(test2.user, '')
+        self.assertEqual(test2.hashtag, '')
+        self.assertEqual(test2.url, '')
 
         
